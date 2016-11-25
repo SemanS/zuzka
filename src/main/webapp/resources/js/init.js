d00ce7029cd1bf16ecba406abfdadd03c86f1ba2
Slavo Seman <Slavosmn@gmail.com>
2016-11-25 19:45:36 +0100

    Layout done

--- src/main/webapp/resources/js/init.js
+++ src/main/webapp/resources/js/init.js
@@ -2,42 +2,7 @@
 
 
     $(function () {
-
-        /*$("#demosMenu").change(function () {
-         window.location.href = $(this).find("option:selected").attr("id") + '.html';
-         });*/
-
-        $('.masthead')
-            .visibility({
-                once: false,
-                onBottomPassed: function() {
-                    $('.fixed.menu').transition('fade in');
-                },
-                onBottomPassedReverse: function() {
-                    $('.fixed.menu').transition('fade out');
-                }
-            })
-        ;
-
-        $('.main.menu').visibility({
-            type: 'fixed'
-        });
-        $('.overlay').visibility({
-            type: 'fixed',
-            offset: 80
-        });
-
-
-        $('.overlay.example .overlay')
-            .visibility({
-                type: 'fixed',
-                offset: 15 // give some space from top of screen
-            })
-        ;
-        /*$('.slider').slider({full_width: true});
-         $('.button-collapse').sideNav();
-         $('ul.tabs').tabs();
-         $('.scrollspy').scrollSpy();*/
+        
         $('.special.cards .image').dimmer({
             on: 'hover'
         });
